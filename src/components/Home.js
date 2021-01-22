2bf15b345e37729fff0ab72670259240cb18b772
pesdesigner <pauloeduardo.dg@outlook.com>
2021-01-21 21:27:17 -0300

    Route - add components page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import image from "../pexels.jpg";
+export default function Home(){
+    return (
+        <main>
+            <img src={image} alt="PhotoPexels" className="absolute object-cover w-full h-full"/>;
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+                Hello! I'm DevPes.</h1>
+            </section>
+        </main>    
+    )
+}
