296d9c0837017318c08dc80b20f096144bc12fc2
pesdesigner <pauloeduardo.dg@outlook.com>
2021-01-22 23:59:05 -0300

    colors

--- src/components/About.js
+++ src/components/About.js
@@ -29,7 +29,7 @@ export default function About(){
         <main className="relative">
             <img src={photo} alt="PhotoAbout" className="absolute w-full" />
             <div className="p-10 lg:pt-48 container mx-auto relative">
-                <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+                <section className="bg-blue-600 rounded-lg shadow-2xl lg:flex p-20">
                     <img src={urlFor(author.authorImage).url()} 
                     className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
                     alt={author.name}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,30 +4,30 @@ import { SocialIcon } from "react-social-icons";
 
 export default function NavBar(){
     return (
-        <header className="bg-red-600">
+        <header className="bg-blue-600">
             <div className="container mx-auto flex justify-between">
                 <nav className="flex">
                     <NavLink to="/" 
                     exact
                     activeClassName="text-white"
-                    className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-gree-800 text-4xl font-bold cursive tracking-widest">
+                    className="inflex-flex items-center py-6 px-3 mr-4 text-white-200 hover:text-green-200 text-4xl font-bold cursive tracking-widest">
                         DevPes
                     </NavLink>
                     <NavLink to="/post"
-                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-                        activeClassName="text-red-100 bg-red-700"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-gray-100"
+                        activeClassName="text-blue-700 bg-green-400"
                     >
                         Blog Posts
                     </NavLink>
                     <NavLink to="/project"
-                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-                        activeClassName="text-red-100 bg-red-700"
+                    className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-yelow-800"
+                        activeClassName="text-blue-700 bg-green-400"
                     >  
                         Projects
                     </NavLink>
                     <NavLink to="/about"
-                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-                        activeClassName="text-red-100 bg-red-700"
+                    className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-yelow-800"
+                        activeClassName="text-blue-700 bg-green-400"
                     >
                         About Me!
                     </NavLink>
